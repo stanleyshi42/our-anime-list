a7b0235190f2ba9a218e22d381831c8d830525bd
stanleyshi42 <stanleyshi42@gmail.com>
2024-07-08 12:58:34 -0700

    starter code

---
+++ backend/src/main/java/com/example/our_anime_list/OurAnimeListApplication.java
@@ -0,0 +1,13 @@
+package com.example.our_anime_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurAnimeListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurAnimeListApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/our_anime_list/OurAnimeListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.our_anime_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurAnimeListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
