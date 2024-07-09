63800fba66a9bddc65d1ca5948865a418d2e0c00
pokeman450 <tman45048@gmail.com>
2024-07-09 15:42:33 -0400

    Logs & testing

--- backend/src/main/java/com/example/our_anime_list/controller/UserController.java
+++ backend/src/main/java/com/example/our_anime_list/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.our_anime_list.controller;
 
 import com.example.our_anime_list.entity.User;
 import com.example.our_anime_list.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,16 +13,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+<<<<<<< Updated upstream
+=======
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+>>>>>>> Stashed changes
     @Autowired
     UserService service;
 
     @PostMapping()
+<<<<<<< Updated upstream
     public User addUser(@RequestBody User user){
         return service.addUser(user);
     }
     @GetMapping("/{userId}")
     public ArrayList<User> getUsersById(@PathVariable long userid){
         return (ArrayList<User>) service.getUserByUserId(userid);
+=======
+    public User addUser(@RequestBody User user) {
+        LOGGER.info("Adding user: {}",user);
+        return userService.addUser(user);
+>>>>>>> Stashed changes
     }
 
     @GetMapping("/user/{username}")
---
+++ backend/src/test/java/com/example/our_anime_list/ControllerTest/UserTest.java
@@ -0,0 +1,5 @@
+package com.example.our_anime_list.ControllerTest;
+
+public class UserTest {
+    
+}
