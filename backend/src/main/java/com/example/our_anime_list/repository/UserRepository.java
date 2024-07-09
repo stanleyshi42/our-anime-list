dfcb91a0ef643dd411f1f012199aea20b98e8919
pokeman450 <tman45048@gmail.com>
2024-07-09 10:28:35 -0400

    update

--- backend/src/main/java/com/example/our_anime_list/repository/UserRepository.java
+++ backend/src/main/java/com/example/our_anime_list/repository/UserRepository.java
@@ -1,11 +1,10 @@
 package com.example.our_anime_list.repository;
 
-import com.example.our_anime_list.entity.Users;
+import com.example.our_anime_list.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<Users, Long>{
-    List<Users> findByUserId(Long userId);
+public interface UserRepository extends JpaRepository<User, Long>{
+    List<User> findByUserId(Long userId);
 }
