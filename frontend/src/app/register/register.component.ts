a36734fa48af040e2daf3d54764fa3eaf5ec7c0a
stanleyshi42 <stanleyshi42@gmail.com>
2024-07-19 12:10:58 -0700

    implemented Register component functionality

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  username = '';
+  password = '';
+  invalidUsername = false;
 
+  constructor(private router: Router, private service: UserService) {}
+
+  register() {
+    if (this.username == '' || this.password == '') return;
+
+    // POST request for new user
+    this.service.register(this.username, this.password).subscribe(
+      (data) => {
+        // After registering, log the user in
+        this.service.login(this.username, this.password).subscribe(
+          (jwt) => {
+            localStorage.setItem('jwt', jwt);
+            this.router.navigateByUrl(''); // Redirect to home
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      },
+      (error) => {
+        console.log(error);
+        this.invalidUsername = true;
+      }
+    );
+  }
 }
