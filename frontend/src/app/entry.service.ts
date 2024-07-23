bb2d2dc654d4518c337b6f004815719f0b189eac
pokeman450 <tman45048@gmail.com>
2024-07-23 12:35:05 -0400

    Updated

--- frontend/src/app/entry.service.ts
+++ frontend/src/app/entry.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Entry } from './entry.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EntryService {
+  private uri = 'http://localhost:8080/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  addEntry(entry: Entry): Observable<any> {
+    return this.http.post<any>(this.uri + 'entry', entry);
+  }
+
+  getListByUserId(id: number): Observable<any> {
+    return this.http.get<any>(this.uri + 'users/' + id + '/list');
+  }
+
+  getFavoritesByUserId(id: number): Observable<any> {
+    return this.http.get<any>(this.uri + 'users/' + id + '/favorites');
+  }
+
+  updateEntry(entry: Entry): Observable<any> {
+    return this.http.put<any>(this.uri + 'entry', entry);
+  }
+
+  deleteEntryById(id: number): Observable<any> {
+    return this.http.delete<any>(this.uri + 'entry/' + id);
+  }
 }
--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { EntryService } from '../entry.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Entry } from '../entry.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  private route = inject(ActivatedRoute);
+  id!: number;
+  username!: any;
+  jwt = localStorage.getItem('jwt');
+  authorized: boolean = false; // Check if logged in user matches this list's user
+
+  constructor(
+      private router: Router,
+      private entryService: EntryService,
+      private userService: UserService
+    ) {}
+
+   ngOnInit() {
+       this.id = Number(this.route.snapshot.paramMap.get('id')); // Get user ID from URL route
+       this.username = this.getUserById(this.id);
+     }
+
+   parseJwt(token: any) {
+       return JSON.parse(atob(token.split('.')[1]));
+     }
+
+  getUserById(id: number) {
+      this.userService.getUserById(id).subscribe((data) => {
+        this.username = data.username;
+
+        // Check if logged in user matches this list's user
+        const parsedJwt = this.parseJwt(this.jwt);
+        if (parsedJwt.sub == this.username) this.authorized = true;
+      });
+    }
+
+
+
 
 }
